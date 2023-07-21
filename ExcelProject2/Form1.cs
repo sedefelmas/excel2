e4f2010137547d654e744cfa1d9d14514269b3d3
Sedef Elmas <sedef.elmas@yupana.com.tr>
2023-07-21 01:17:18 -0700

    Son güncelleme

--- ExcelProject2/Form1.cs
+++ ExcelProject2/Form1.cs
@@ -20,7 +20,7 @@ namespace ExcelProject2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string strFilePath = string.Empty;
+            string filePath = string.Empty;
 
             openFileDialog1.InitialDirectory = Application.StartupPath;
             openFileDialog1.FileName = "*.xlsx";
@@ -28,24 +28,24 @@ namespace ExcelProject2
 
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                strFilePath = openFileDialog1.FileName;
+                filePath = openFileDialog1.FileName;
                 Excel.Application xlApp = new Excel.Application();
-                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(strFilePath);
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(filePath);
                 Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
                 Excel.Range xlRange = xlWorksheet.UsedRange;
 
                 int rowCount = xlRange.Rows.Count;
                 int colCount = xlRange.Columns.Count;
-
-                //iterate over the rows and columns and print to the console as it appears in the file
-                //excel is not zero based!!
+                string cellValue;
                 for (int i = 1; i <= rowCount; i++)
                 {
                     for (int j = 1; j <= colCount; j++)
                     {
-                        //write the value to the console
                         if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
-                            listBox1.Items.Add(xlRange.Cells[i, j].Value2.ToString());
+                        {
+                            cellValue = xlRange.Cells[i, j].Value2.ToString();
+                            listBox1.Items.Add(cellValue);
+                        }
                     }
                 }
             }
